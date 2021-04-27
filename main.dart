bd6c2ec1c339f41d9b10d3617515bbcfcca4e170
Jagan <54259481+Jagan663@users.noreply.github.com>
2021-04-27 23:13:53 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+import './quiz.dart';
+import './result.dart';
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'Which is the national animal of India?',
+      'answers': [
+        {'text': 'Tiger', 'score': 10},
+        {'text': 'Cheeta', 'score': 0},
+        {'text': 'Elephent', 'score': 0},
+        {'text': 'Lion', 'score': 0},
+      ],
+    },
+    {
+      'questionText': 'Who is the president of India?',
+      'answers': [
+        {'text': 'Modi', 'score': 10},
+        {'text': 'Amith', 'score': 0},
+        {'text': 'Rahul', 'score': 0},
+        {'text': 'Indira', 'score': 0},
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ],
+    },
+  ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    // var aBool = true;
+    // aBool = false;
+
+    _totalScore += score;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // var dummy = const ['Hello'];
+    // dummy.add('Max');
+    // print(dummy);
+    // dummy = [];
+    // questions = []; // does not work if questions is a const
+
+    return MaterialApp(
+        home: Container(
+      alignment: Alignment.center,
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(100, 100, 181, 100),
+        appBar: AppBar(
+          title: Text('Quiz App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
+      ),
+    ));
+  }
+}
